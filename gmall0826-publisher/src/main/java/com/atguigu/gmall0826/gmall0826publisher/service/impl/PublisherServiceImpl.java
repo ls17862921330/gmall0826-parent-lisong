21f434551d1c7ba6c6a2f7a7a0d5ddd1827cd870
lisong <1074220898@qq.com>
2020-02-11 10:07:08 +0800

    分时查询

--- gmall0826-publisher/src/main/java/com/atguigu/gmall0826/gmall0826publisher/service/impl/PublisherServiceImpl.java
+++ gmall0826-publisher/src/main/java/com/atguigu/gmall0826/gmall0826publisher/service/impl/PublisherServiceImpl.java
@@ -2,9 +2,13 @@ package com.atguigu.gmall0826.gmall0826publisher.service.impl;
 
 import com.atguigu.gmall0826.gmall0826publisher.mapper.DauMapper;
 import com.atguigu.gmall0826.gmall0826publisher.service.PublisherService;
+import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,16 +25,39 @@ public class PublisherServiceImpl implements PublisherService {
     }
 
     @Override
-    public Map getDauHourCount(String date) {
-
-        List<Map> maps = dauMapper.selectDauHourCount(date);
+    public Map getDauHourCount(String date) {  //调整变换结构
+        //   maplist ===> [{"loghour":"12","ct",500},{"loghour":"11","ct":400}......]
 
         Map resultMap = new HashMap();
 
-        for (Map map : maps) {
-            resultMap.put(map.get("LOGHOUR"), map.get("CT"));
+        List<Map> mapList = dauMapper.selectDauHourCount(date);
+        // hourCountMap ==> {"12":500,"11":400,......}
+        Map todayMap=new HashMap();
+        for (Map map : mapList) {
+            todayMap.put ( map.get("LOGHOUR"),map.get("CT"));
         }
 
+        Map yesterdayMap = getYesterdayDauHourCount(date);
+
+        resultMap.put("yesterday",yesterdayMap);
+        resultMap.put("today",todayMap);
+
         return resultMap;
+    }
+
+    private Map getYesterdayDauHourCount(String date) {
+
+        Map hourCountMap=new HashMap();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date yes = DateUtils.addDays(sdf.parse(date), -1);
+            List<Map> mapList = dauMapper.selectDauHourCount(sdf.format(yes));
+            for (Map map : mapList) {
+                hourCountMap.put ( map.get("LOGHOUR"),map.get("CT"));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return hourCountMap;
     }
 }
